1f0059d32c5873194da949f5be82e3ae18ee0134
michaelfoy <mikkfoy@gmail.com>
2016-09-12 12:22:26 +0100

    step05 complete

--- app/src/main/java/com/mfoy/donation/Donate.java
+++ app/src/main/java/com/mfoy/donation/Donate.java
@@ -4,11 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+
 
 public class Donate extends AppCompatActivity {
     private Button donateButton;
+    private RadioGroup paymentMethod;
+    private ProgressBar  progressBar;
+    private NumberPicker amountPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,13 @@ public class Donate extends AppCompatActivity {
         setContentView(R.layout.activity_donate);
 
         donateButton = (Button) findViewById(R.id.donateButton);
-        Log.v("Donate", "got the donate button");
+        paymentMethod = (RadioGroup)   findViewById(R.id.paymentMethod);
+        progressBar   = (ProgressBar)  findViewById(R.id.progressBar);
+        amountPicker  = (NumberPicker) findViewById(R.id.amountPicker);
+
+        amountPicker.setMaxValue(1000);
+        amountPicker.setMinValue(0);
+
         if (donateButton != null)
         {
             Log.v("Donate", "Really got the donate button");
@@ -34,4 +48,22 @@ public class Donate extends AppCompatActivity {
     {
         Log.v("Donate", "Donate Pressed!");
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
