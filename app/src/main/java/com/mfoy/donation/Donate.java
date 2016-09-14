9f7f33b5f10744e4c836a4cad67f588f602d0fdc
michaelfoy <mikkfoy@gmail.com>
2016-09-14 16:58:51 +0100

    exercise2 complete

--- app/src/main/java/com/mfoy/donation/Donate.java
+++ app/src/main/java/com/mfoy/donation/Donate.java
@@ -1,5 +1,6 @@
 package com.mfoy.donation;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,14 +11,25 @@ import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class Donate extends AppCompatActivity {
     private Button donateButton;
     private RadioGroup paymentMethod;
-    private ProgressBar  progressBar;
+    private ProgressBar progressBar;
     private NumberPicker amountPicker;
+    private EditText numberText;
     private int totalDonated = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +37,21 @@ public class Donate extends AppCompatActivity {
         setContentView(R.layout.activity_donate);
 
         donateButton = (Button) findViewById(R.id.donateButton);
-        paymentMethod = (RadioGroup)   findViewById(R.id.paymentMethod);
-        progressBar   = (ProgressBar)  findViewById(R.id.progressBar);
-        amountPicker  = (NumberPicker) findViewById(R.id.amountPicker);
+        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
+        numberText = (EditText) findViewById(R.id.numberText);
 
         amountPicker.setMaxValue(1000);
         amountPicker.setMinValue(0);
         progressBar.setMax(1000);
 
-        if (donateButton != null)
-        {
+        if (donateButton != null) {
             Log.v("Donate", "Really got the donate button");
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -46,27 +61,73 @@ public class Donate extends AppCompatActivity {
         return true;
     }
 
-    public void donateButtonPressed (View view) {
-        totalDonated += amountPicker.getValue();
+    public void donateButtonPressed(View view) {
+        if (amountPicker.getValue() != 0) {
+            totalDonated += amountPicker.getValue();
+        } else {
+            String text = numberText.getText().toString();
+            if (text != "") {
+                totalDonated += Integer.parseInt(text);
+            }
+        }
         progressBar.setProgress(totalDonated);
         String method = paymentMethod.getCheckedRadioButtonId() == R.id.PayPal ? "PayPal" : "Direct";
+        Log.v("Donate", "Total donated increased to " + totalDonated + " by: " + method);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
+        if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Donate Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.mfoy.donation/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Donate Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.mfoy.donation/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
 
