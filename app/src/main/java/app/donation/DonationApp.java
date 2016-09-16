d8f9fd8c533469e108e4088d342c181f8fcce131
michaelfoy <mikkfoy@gmail.com>
2016-09-16 11:56:52 +0100

    lab02-step06 complete

--- app/src/main/java/app/donation/DonationApp.java
+++ app/src/main/java/app/donation/DonationApp.java
@@ -2,9 +2,33 @@ package app.donation;
 
 import android.app.Application;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DonationApp extends Application
 {
+    public final int       target       = 10000;
+    public int             totalDonated = 0;
+    public List<Donation> donations    = new ArrayList<Donation>();
+
+    public boolean newDonation(Donation donation)
+    {
+        boolean targetAchieved = totalDonated > target;
+        if (!targetAchieved)
+        {
+            donations.add(donation);
+            totalDonated += donation.amount;
+        }
+        else
+        {
+            Toast toast = Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        return targetAchieved;
+    }
+
     @Override
     public void onCreate()
     {
